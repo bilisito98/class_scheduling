8683ceda4ea729ad4cf89eba24e106ea4201e5e4
william <wilsher987@gmail.com>
2025-07-24 15:47:27 -0500

    commitfixing

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const PORT = 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+});
