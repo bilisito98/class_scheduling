c27064fd45fbacb893ed82e85fad0ac1a65bbb96
william <wilsher987@gmail.com>
2025-07-24 16:43:10 -0500

    fixing express downgrade + wildcard

--- server.js
+++ server.js
@@ -10,14 +10,12 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Servir los archivos estáticos generados por Vite
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Todas las rutas deben devolver el index.html para React Router
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Frontend corriendo en http://localhost:${PORT}`);
+  console.log(`✅ Servidor frontend corriendo en http://localhost:${PORT}`);
 });
